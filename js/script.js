4bac8a812bd883fd4ce1ce18b9c53f15564ba121
Alfred <shadowdante99@gmail.com>
2022-11-17 18:00:57 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"Use script";
+
+ // alert('Hello');
+
+// const result =  confirm('Are you here');
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+// const answer = [];
+
+// answer [0] = prompt('як ваше імья', '');
+//answer [1] = prompt('як ваше Фамілія', '');
+//answer [2] = prompt('як вас по Батькові', '');
+
+console.log('arr' + " - objekct");
+console.log(4 + "5");
+
+let incr = 10, 
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
